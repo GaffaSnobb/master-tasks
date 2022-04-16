d96fdd44a5635f3d9a9c793347f675159c80b20a
Jon Dahl <jon@dira.no>
2022-04-16 13:21:16 +0200

    Upload Sc45 data

---
+++ Sc45/sdpf-sdg/200_states/2hw/complete/016_15n.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+#SBATCH --job-name=Sc457.5- 
+#SBATCH --account=NN9464K 
+## Syntax is d-hh:mm:ss 
+#SBATCH --time=0-02:00:00 
+#SBATCH --nodes=64
+#SBATCH --ntasks-per-node=8 
+#SBATCH --cpus-per-task=16 
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=jonkd@uio.no 
+module --quiet purge  
+module load intel/2020b 
+module load Python/3.8.6-GCCcore-10.2.0 
+set -o errexit  
+set -o nounset 
+export OMP_NUM_THREADS=32 
+echo "start running log_Sc45_GCLSTsdpfsdgix5pn_j15n.txt ..."
+cat > Sc45_GCLSTsdpfsdgix5pn_15n.input <<EOF
+&input
+  beta_cm = 10.0
+  eff_charge = 1.5, 0.5, 
+  fn_int = "GCLSTsdpfsdgix5pn.snt"
+  fn_ptn = "Sc45_GCLSTsdpfsdgix5pn_n.ptn"
+  fn_save_wave = "Sc45_GCLSTsdpfsdgix5pn_j15n.wav"
+  gl = 1.0, 0.0, 
+  gs = 4.189, -2.869, 
+  hw_type = 2
+  is_double_j = .true.
+  max_lanc_vec = 350
+  maxiter = 300
+  mode_lv_hdd = 0
+  mtot = 15
+  n_block = 8
+  n_eigen = 200
+  n_restart_vec = 300
+&end
+EOF
+mpiexec ./kshell.exe Sc45_GCLSTsdpfsdgix5pn_15n.input > log_Sc45_GCLSTsdpfsdgix5pn_j15n.txt  
+
+rm -f tmp_snapshot_Sc45_GCLSTsdpfsdgix5pn_n.ptn_15_* tmp_lv_Sc45_GCLSTsdpfsdgix5pn_n.ptn_15_* Sc45_GCLSTsdpfsdgix5pn_15n.input 
+
+
---
+++ Sc45/sdpf-sdg/200_states/2hw/complete/027_tr_3p_3n.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+#SBATCH --job-name=3p3n
+#SBATCH --account=NN9464K 
+## Syntax is d-hh:mm:ss 
+#SBATCH --time=0-00:30:00
+#SBATCH --nodes=32
+#SBATCH --ntasks-per-node=8 
+#SBATCH --cpus-per-task=16 
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=jonkd@uio.no 
+module --quiet purge  
+module load intel/2020b 
+module load Python/3.8.6-GCCcore-10.2.0 
+set -o errexit  
+set -o nounset 
+export OMP_NUM_THREADS=32 
+echo "start running log_Sc45_GCLSTsdpfsdgix5pn_tr_j3p_j3n.txt ..."
+cat > Sc45_GCLSTsdpfsdgix5pn_3p_3n.input <<EOF
+&input
+  fn_int   = "GCLSTsdpfsdgix5pn.snt"
+  fn_ptn_l = "Sc45_GCLSTsdpfsdgix5pn_p.ptn"
+  fn_ptn_r = "Sc45_GCLSTsdpfsdgix5pn_n.ptn"
+  fn_load_wave_l = "Sc45_GCLSTsdpfsdgix5pn_j3p.wav"
+  fn_load_wave_r = "Sc45_GCLSTsdpfsdgix5pn_j3n.wav"
+  hw_type = 2
+  eff_charge = 1.5, 0.5
+  gl = 1.0, 0.0
+  gs = 4.189, -2.869
+&end
+EOF
+mpiexec ./transit.exe Sc45_GCLSTsdpfsdgix5pn_3p_3n.input > log_Sc45_GCLSTsdpfsdgix5pn_tr_j3p_j3n.txt  
+
+rm -f Sc45_GCLSTsdpfsdgix5pn_3p_3n.input
+
+
