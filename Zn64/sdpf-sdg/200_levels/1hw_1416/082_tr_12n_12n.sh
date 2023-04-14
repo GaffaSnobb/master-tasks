0d6c280c04766e451f320866ca19830f1e7d44aa
Jon Dahl <jon@dira.no>
2023-04-14 14:31:55 +0900

    Add files generated by kshell_ui

---
+++ Zn64/sdpf-sdg/200_levels/1hw_1416/082_tr_12n_12n.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+#SBATCH --job-name=Zn64_gs8 
+#SBATCH --account=NN9464K 
+## Syntax is d-hh:mm:ss 
+#SBATCH --time=0-03:00:00 
+#SBATCH --nodes=64
+#SBATCH --ntasks-per-node=8 
+#SBATCH --cpus-per-task=16 
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=jonkd@uio.no 
+module --quiet purge  
+module load intel/2020b 
+module load Python/3.8.6-GCCcore-10.2.0 
+set -o errexit  
+set -o nounset 
+export OMP_NUM_THREADS=32 
+echo "start running log_Zn64_gs8_tr_j12n_j12n.txt ..."
+cat > Zn64_gs8_12n_12n.input <<EOF
+&input
+  fn_int   = "gs8.snt"
+  fn_ptn_l = "Zn64_gs8_n.ptn"
+  fn_ptn_r = "Zn64_gs8_n.ptn"
+  fn_load_wave_l = "Zn64_gs8_j12n.wav"
+  fn_load_wave_r = "Zn64_gs8_j12n.wav"
+  hw_type = 2
+  eff_charge = 1.5, 0.5
+  gl = 1.0, 0.0
+  gs = 4.189, -2.869
+&end
+EOF
+mpiexec ./transit.exe Zn64_gs8_12n_12n.input > log_Zn64_gs8_tr_j12n_j12n.txt  
+
+rm -f Zn64_gs8_12n_12n.input
+
+
---
+++ Zn64/sdpf-sdg/200_levels/1hw_1416/2n.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+#SBATCH --job-name=2n 
+#SBATCH --account=NN9464K 
+## Syntax is d-hh:mm:ss 
+#SBATCH --time=0-05:00:00 
+#SBATCH --nodes=64
+#SBATCH --ntasks-per-node=8 
+#SBATCH --cpus-per-task=16 
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=jonkd@uio.no 
+module --quiet purge  
+module load intel/2020b 
+module load Python/3.8.6-GCCcore-10.2.0 
+set -o errexit  
+set -o nounset 
+export OMP_NUM_THREADS=32 
+echo "start running log_Zn64_gs8_j4n.txt ..."
+cat > Zn64_gs8_4n.input <<EOF
+&input
+  beta_cm = 10.0
+  eff_charge = 1.5, 0.5, 
+  fn_int = "gs8.snt"
+  fn_ptn = "Zn64_gs8_n.ptn"
+  fn_save_wave = "Zn64_gs8_j4n.wav"
+  gl = 1.0, 0.0, 
+  gs = 4.189, -2.869, 
+  hw_type = 2
+  is_double_j = .true.
+  max_lanc_vec = 350
+  maxiter = 300
+  mode_lv_hdd = 0
+  mtot = 4
+  n_block = 8
+  n_eigen = 200
+  n_restart_vec = 300
+  orbs_ratio = 4, 16, 
+&end
+EOF
+mpiexec ./kshell.exe Zn64_gs8_4n.input > log_Zn64_gs8_j4n.txt  
+
+rm -f tmp_snapshot_Zn64_gs8_n.ptn_4_* tmp_lv_Zn64_gs8_n.ptn_4_* Zn64_gs8_4n.input 
+
+
