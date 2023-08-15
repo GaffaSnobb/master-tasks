f92babc9e8b4eedcb6e468187195c4b89fa3dc7f
Jon Dahl <jon@dira.no>
2023-08-15 18:24:13 +0900

    Add compare_varying_number_of_levels_per_j

--- V50/V50.py
+++ V50/V50.py
@@ -9,6 +9,50 @@ BIN_WIDTH = 0.2
 EX_MIN = 5.4    # Lower lim. from the 2006 experiment?
 EX_MAX = 9.3334 # S(n)
 
+def compare_varying_number_of_levels_per_j():
+    n_levels = [60, 100, 200]
+    colors = ["cyan", "dodgerblue", "blue"]
+    fig_0, ax_0 = plt.subplots()
+    fig_1, ax_1 = plt.subplots()
+    
+    res = ksutil.loadtxt(
+        path = "sdpf-mu/200_levels/1hw/summary_V50_sdpf-mu_000.txt",
+        load_and_save_to_file = True,
+        old_or_new = "new"
+    )
+
+    for levels, color in zip(n_levels, colors):
+        bins_gsf_M1, gsf_M1 = res.gsf(
+            bin_width = BIN_WIDTH,
+            Ex_min = EX_MIN,
+            Ex_max = EX_MAX,
+            multipole_type = "M1",
+            include_n_levels = levels,
+            plot = False
+        )
+        ax_0.step(bins_gsf_M1, gsf_M1, label=f"{levels} levels per " + r"$j^{\pi}$", color=color)
+
+        bins_nld, nld = res.nld(
+            bin_width = BIN_WIDTH,
+            include_n_levels = levels,
+            plot = False
+        )
+        ax_1.step(bins_nld, nld, label=f"{levels} levels per " + r"$j^{\pi}$", color=color)
+    
+    ax_0.set_yscale('log')
+    ax_0.set_xlabel(r"$E_{\gamma}$ [MeV]")
+    ax_0.set_ylabel(r"GSF [MeV$^{-3}$]")
+    ax_0.set_title(r"$^{50}$V, sdpf-mu, M1")
+    ax_0.legend()
+    fig_0.savefig("V50_gsf_sdpf-mu_var_n_levels_E1.png", dpi=300)
+
+    ax_1.set_xlabel(r"$E$ [MeV]")
+    ax_1.set_ylabel(r"NLD [MeV$^{-1}$]")
+    ax_1.set_title(r"$^{50}$V, sdpf-mu")
+    ax_1.legend()
+    fig_1.savefig("V50_nld_sdpf-mu_var_n_levels.png", dpi=300)
+    plt.show()
+
 def gsf_compare_experimental_and_sdpfmu():
     """
     Compare the numerical and experimental gamma strength functions of
@@ -50,7 +94,7 @@ def gsf_compare_experimental_and_sdpfmu():
     # ax.set_title(r"$^{50}$V, SDPF-MU, 200 levels per $j^{\pi}$, $1 \hbar \omega$")
     ax.yaxis.set_label_coords(-0.1, 0.5)
     ax.legend()
-    fig.savefig("v50_gsf_kshell_vs_experimental_sdpf-mu_200_levels.png", dpi=300)
+    # fig.savefig("v50_gsf_kshell_vs_experimental_sdpf-mu_200_levels.png", dpi=300)
     plt.show()
 
 def gsf_M1_compare_experimental_and_gxpf1a_and_sdpfmu():
@@ -105,10 +149,11 @@ def gsf_M1_compare_experimental_and_gxpf1a_and_sdpfmu():
     ax["lower"].step(bins_M1_sdpfmu, gsf_M1_diff, label=r"Rel. difference", color="firebrick")
     ax["lower"].legend()
 
-    fig.savefig("v50_gsf_exp_vs_sdpf-mu_vs_gxpf1a_200_levels.png", dpi=300)
+    # fig.savefig("v50_gsf_exp_vs_sdpf-mu_vs_gxpf1a_200_levels.png", dpi=300)
     plt.show()
 
 if __name__ == "__main__":
-    gsf_compare_experimental_and_sdpfmu()
-    gsf_M1_compare_experimental_and_gxpf1a_and_sdpfmu()
+    # gsf_compare_experimental_and_sdpfmu()
+    # gsf_M1_compare_experimental_and_gxpf1a_and_sdpfmu()
+    compare_varying_number_of_levels_per_j()
     pass
